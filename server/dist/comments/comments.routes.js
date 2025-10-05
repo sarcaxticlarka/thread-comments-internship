95727291c76f20bda51d1cfb2f115ed0bf800c6a
underxcore <osmsad786@gmail.com>
2025-10-05 16:58:44 +0530

    fix errors

--- server/dist/comments/comments.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getComments, createComment, likeComment, replyComment } from '../comments/comments.controllers.js';
-const router = express.Router();
-router.get('/', getComments);
-router.post('/', createComment);
-router.post('/:id/like', likeComment);
-router.post("/:id/reply", replyComment);
-export default router;
